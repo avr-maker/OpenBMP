0277046accb06780184dff2e7174cd32528b47dd
groknut <184639205+groknut@users.noreply.github.com>
2025-11-06 22:52:46 +0800

    add InfoHeader Structure

---
+++ open-bmp.cpp
@@ -0,0 +1,30 @@
+
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+// #include <cstdint>
+
+struct BITMAP_FILEHEADER
+{
+    uint16_t bfTybe;
+    uint32_t bfSize;
+    uint16_t bfreserved1;
+    uint16_t bfreserved2;
+    uint32_t bfOffBits;
+};
+
+struct BITMAP_INFOHEADER
+{
+	uint32_t size;
+	uint32_t width;
+	uint32_t height;
+	uint16_t planes;
+	uint16_t bitCount;
+	uint32_t compression;
+	uint32_t imageSize;
+	uint32_t XpixelsPerM;
+	uint32_t YpixelsPerM;
+	uint32_t colorsUsed;
+	uint32_t colorsImportant;
+};
