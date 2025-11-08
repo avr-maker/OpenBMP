4ee5a5dce289a8a88eae8070ebaf91063bcd754a
groknut <184639205+groknut@users.noreply.github.com>
2025-11-08 09:56:18 +0800

    create obmp.h & obmp.cpp files

---
+++ obmp.cpp
@@ -0,0 +1,35 @@
+
+
+#include "obmp.h"
+
+OpenBMP::OpenBMP(const std::string& filename)
+{
+	std::ifstream in(filename, ios::binary);
+
+	if (!in.is_open())
+		throw OpenBMPError();
+
+	in.read(
+		reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader)		
+	);
+	in.read(
+		reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader) 
+	);
+
+	if (fileHeader.bfTybe != 0x4D42)
+		throw OpenBMPError();
+
+	if (infoHeader.bitCount != 24)
+		throw OpenBMPFormatError();			
+}
+
+std::pair<int, int> OpenBMP::shape()
+{
+	return std::make_pair(infoHeader.height, infoHeader.width);
+}
+
+ostream& operator << (ostream& out, const std::pair<int ,int>& shape)
+{
+	out << "(" << shape.first << ", " << shape.second << ")";
+	return out;
+}
---
+++ obmp.h
@@ -0,0 +1,63 @@
+
+#ifndef _OBMP_H_
+#define _OBMP_H_
+
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <string>
+
+using namespace std;
+
+#pragma pack(push,1)
+
+struct BITMAP_FILEHEADER
+{
+    uint16_t bfTybe;
+    uint32_t bfSize;
+    uint16_t bfreserved1;
+    uint16_t bfreserved2;
+    uint32_t bfOffBits;
+};
+
+struct BITMAP_INFOHEADER
+{
+	uint32_t size;
+	uint32_t width;
+	uint32_t height;
+	uint16_t planes;
+	uint16_t bitCount;
+	uint32_t compression;
+	uint32_t imageSize;
+	uint32_t XpixelsPerM;
+	uint32_t YpixelsPerM;
+	uint32_t colorsUsed;
+	uint32_t colorsImportant;
+};
+
+struct BITMAP_COLORTABLE
+{
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;	
+};
+#pragma pack(pop)
+
+class OpenBMP
+{
+private:
+	BITMAP_FILEHEADER fileHeader;
+	BITMAP_INFOHEADER infoHeader;
+	bool readFile(const std::string& filename);
+	int width, height;
+public:
+	OpenBMP(const std::string& filename);	
+	std::pair<int, int> shape();
+};
+
+class OpenBMPError {};
+class OpenBMPFormatError {};
+
+ostream& operator << (ostream& out, const std::pair<int ,int>& shape);
+
+#endif
